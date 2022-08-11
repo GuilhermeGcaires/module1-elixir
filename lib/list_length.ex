f53740f0bfa16f76c6e2c13643d2438973c37769
guilhermegcaires <guilhermegcaires@gmail.com>
2022-08-11 12:21:24 -0300

    Finished task

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: sum(list, 0)
+
+  def sum([], acc), do: acc
+
+  def sum([_head | tail], acc) do
+    acc = acc + 1
+    sum(tail, acc)
   end
 end
